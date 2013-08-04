668a14b5f5a9fc3b98956bbd15e144a0812a346c
shengzhc <shengzhc@usc.edu>
2013-08-04 16:52:14 -0700

    initializing iCoffee

---
+++ iCoffee/iCoffee/ICAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICAppDelegate.h
+//  iCoffee
+//
+//  Created by Shengzhe Chen on 8/4/13.
+//  Copyright (c) 2013 iCoffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iCoffee/iCoffee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iCoffee
+//
+//  Created by Shengzhe Chen on 8/4/13.
+//  Copyright (c) 2013 iCoffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ICAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICAppDelegate class]));
+    }
+}
