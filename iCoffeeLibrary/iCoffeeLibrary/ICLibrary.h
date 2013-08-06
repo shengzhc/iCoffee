42e874bcbd77bc26047ba6ba5bb64e449f715198
shengzhc <shengzhc@usc.edu>
2013-08-05 22:52:37 -0700

    merge

---
+++ iCoffeeLibrary/iCoffeeLibrary/ICLibrary.h
@@ -0,0 +1,28 @@
+//
+//  ICLibrary.h
+//  iCoffeeLibrary
+//
+//  Created by Shengzhe Chen on 8/5/13.
+//  Copyright (c) 2013 iCoffee. All rights reserved.
+//
+
+#ifndef iCoffeeLibrary_ICLibrary_h
+#define iCoffeeLibrary_ICLibrary_h
+
+#import "ICLog.h"
+#import "ICNotifications.h"
+#import "NSArray+Extension.h"
+#import "NSDate+ICServices.h"
+#import "NSNumber+ICServices.h"
+#import "NSObject+ICServices.h"
+#import "NSString+ICServices.h"
+#import "UIColor+ICServices.h"
+#import "UIColor+ICSwatch.h"
+#import "UIFont+ICServices.h"
+#import "UIImage+ICServices.h"
+#import "UIImageView+ICServices.h"
+#import "UILabel+ICServices.h"
+#import "UIView+ICServices.h"
+#import "ICGeometry.h"
+
+#endif
