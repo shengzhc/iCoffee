b6c4fdc4bb4447c85f7a3bb3540c0cc819361c6b
Marty <marty@ubuntu.(none)>
2013-08-17 00:14:13 -0700

    initial backend

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backend::Application.config.secret_token = 'c9c7cfe663af97cbb980ace83968c85ac5e1a706d69ac1828361a56063382a6027626b48e8593a72ed5a637d35e98634234f4c7f49293b1f898c1e736ca3f02a'
