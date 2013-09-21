606080572239606a3a21ff655b7e7d08d8d06a19
shengzhc <shengzhc@usc.edu>
2013-09-21 10:29:12 -0700

    update with ios 7

--- iCoffee/iCoffee/View/Cell/ICCollectionViewCell.h
+++ iCoffee/iCoffee/View/Cell/ICCollectionViewCell.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ICCollectionViewCell : UICollectionReusableView
+@interface ICCollectionViewCell : UICollectionViewCell
 
 - (void)setBackgroundImage:(UIImage *)image;
 
--- iCoffee/iCoffee/View/Header/ICHeaderBarView.m
+++ iCoffee/iCoffee/View/Header/ICHeaderBarView.m
@@ -11,11 +11,20 @@
 
 @implementation ICHeaderBarView
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+    }
+    
+    return self;
+}
+
 - (void)drawRect:(CGRect)rect
 {
     UIImage *image = [UIImage imageNamed:@"personalize_bar"];
     [image drawInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-    [self addShadowWithColor:[UIColor blackColor]];
 }
 
 
